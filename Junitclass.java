58ed71d7de2bd1532dc6f8ffaf4ccd79cc088b90
Kvpavankumar1 <121178582+Kvpavankumar1@users.noreply.github.com>
2022-12-23 22:31:58 +0800

    Create Junitclass.java

---
+++ Junitclass.java
@@ -0,0 +1,14 @@
+package pavan;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Junitclass {
+
+	public static void main(String[] args) {
+
+		System.out.print("Hello world");
+		RemoteWebDriver driver;
+		
+	}
+
+}
